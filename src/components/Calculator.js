52a1fba46ec449bba8d204d2b9cf4bc06b3f93d0
Mohamed Jadib <jadibdev@gmail.com>
2022-06-30 12:17:20 +0100

    Refactor code to use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,108 +1,103 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Screen from './Screen';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [State, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (e) => {
-    this.setState((state) => calculate(state, e.target.innerText));
+  const handleClick = (e) => {
+    const display = () => calculate(State, e.target.innerHTML);
+    setState(display);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    let result;
-
-    if (next === null && operation === null && total === null) {
-      result = 0;
-    }
-
-    if (next !== null && operation === null && total === null) {
-      result = next;
-    }
-
-    if (next === null && operation !== null && total !== null) {
-      result = total + operation;
-    }
-
-    if (next !== null && operation !== null && total !== null) {
-      result = total + operation + next;
-    }
-
-    if (next === null && operation === null && total !== null) {
-      result = total;
-    }
-    return (
-      <div className="container">
-        <Screen result={result} />
-        <button type="button" className="item" onClick={this.handleClick}>
-          AC
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          +/-
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          %
-        </button>
-        <button type="button" className="item orange" onClick={this.handleClick}>
-          ÷
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          7
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          8
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          9
-        </button>
-        <button type="button" className="item orange" onClick={this.handleClick}>
-          x
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          4
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          5
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          6
-        </button>
-        <button type="button" className="item orange" onClick={this.handleClick}>
-          +
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          3
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          2
-        </button>
-        <button type="button" className="item" onClick={this.handleClick}>
-          1
-        </button>
-        <button type="button" className="item orange" onClick={this.handleClick}>
-          -
-        </button>
-        <button type="button" className="item zero" onClick={this.handleClick}>
-          0
-        </button>
-        <button type="button" className="item dot" onClick={this.handleClick}>
-          .
-        </button>
-        <button type="button" className="item equals orange" onClick={this.handleClick}>
-          =
-        </button>
-      </div>
-    );
+  let result;
+  const { total, next, operation } = State;
+  if (next === null && operation === null && total === null) {
+    result = 0;
   }
-}
+
+  if (next !== null && total === null) {
+    result = next;
+  }
+
+  if (next === null && operation !== null && total !== null) {
+    result = total + operation;
+  }
+
+  if (next !== null && operation !== null && total !== null) {
+    result = total + operation + next;
+  }
+
+  if (next === null && operation === null && total !== null) {
+    result = total;
+  }
+
+  return (
+    <div className="container">
+      <Screen result={result} />
+      <button type="button" className="item" onClick={handleClick}>
+        AC
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        +/-
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        %
+      </button>
+      <button type="button" className="item orange" onClick={handleClick}>
+        ÷
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        7
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        8
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        9
+      </button>
+      <button type="button" className="item orange" onClick={handleClick}>
+        x
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        4
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        5
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        6
+      </button>
+      <button type="button" className="item orange" onClick={handleClick}>
+        +
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        3
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        2
+      </button>
+      <button type="button" className="item" onClick={handleClick}>
+        1
+      </button>
+      <button type="button" className="item orange" onClick={handleClick}>
+        -
+      </button>
+      <button type="button" className="item zero" onClick={handleClick}>
+        0
+      </button>
+      <button type="button" className="item dot" onClick={handleClick}>
+        .
+      </button>
+      <button type="button" className="item equals orange" onClick={handleClick}>
+        =
+      </button>
+    </div>
+  );
+};
 
 export default Calculator;
